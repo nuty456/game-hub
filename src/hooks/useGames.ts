18ae1432f8de832ac6ad0524eef8f1b8544df643
jbendl <jirka.bendl@gmail.com>
2024-02-21 21:16:42 +0100

    Refactor: Fetching games logic moved to useGames hook
    The logic for fetching games was dispersed within the GameGrid component. This made both the component and its HTTP request handling bloated. This update refactors the GameGrid by moving fetching games logic inside a newly created hook called useGames.

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react';
+import apiClient from "../services/api-client";
+import {CanceledError} from "axios";
+
+interface Game {
+    id: number;
+    name: string;
+}
+
+interface FetchGamesResponse {
+    count: number;
+    results: Game[];
+}
+
+const UseGames = () => {
+    const controller = new AbortController();
+    const [games, setGames] = useState<Game[]>([]);
+    const [error, setError] = useState('');
+    useEffect(() => {
+        apiClient.get<FetchGamesResponse>('/games')
+            .then(res => {
+                setGames(res.data.results);
+            })
+            .catch(err => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
+            });
+        return () => controller.abort();
+    }, []);
+    return {games, error, setGames, setError}
+};
+
+export default UseGames;
