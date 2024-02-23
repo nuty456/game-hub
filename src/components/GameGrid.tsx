d32818a9083f410c760052f9b068146d208dba98
jbendl <jirka.bendl@gmail.com>
2024-02-23 19:20:58 +0100

    Add platform selector to game grid
    Implemented a platform selector for the game grid. This functionality uses a new hook, usePlatforms, to fetch and display a list of platforms as a selectable menu. The PlatformSelector component has been added to the GameGrid display.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,6 +5,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import {Genre} from "../hooks/useGenres";
+import PlatformSelector from "./PlatformSelector";
 interface GameGridProps {
     selectedGenre: Genre | null;
 }
@@ -15,6 +16,7 @@ const GameGrid = ({selectedGenre}: GameGridProps) => {
     return (
         <>
             {error && <Text>{error}</Text>}
+            <PlatformSelector/>
             <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={2} padding="10px">
                 {isLoading && skeletons.map(skeleton => (
                     <GameCardContainer key={skeleton}>
