0b861da0c93e120a932660bf333b3162d3b9f592
jbendl <jirka.bendl@gmail.com>
2024-03-06 16:51:16 +0100

    Add trailer features for game details
    Implemented a new Trailer entity along with a useTrailers hook and GameTrailer component. These enhancements allow for fetching and displaying game trailers on the GameDetailPage. The Trailer entity represents the structure of a Trailer object, the useTrailers hook manages data fetching from the API, and the GameTrailer component is responsible for the UI presentation.

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useTrailers from "../hooks/useTrailers";
+
+interface GameTrailerProps {
+    gameId: number;
+}
+
+const GameTrailer = ({gameId}: GameTrailerProps) => {
+    const {data: trailer, isLoading, error} = useTrailers(gameId);
+    if (isLoading) return null
+    if (error) throw error
+    const first = trailer?.results[0]
+    return first ? (
+        <video
+            poster={first.preview}
+            src={first.data[480]}
+            controls/>
+    ) : null;
+};
+
+export default GameTrailer;
