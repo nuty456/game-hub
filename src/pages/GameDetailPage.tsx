233ce6c64d6f2e5d421c9a6c200e5d337b1a924b
jbendl <jirka.bendl@gmail.com>
2024-03-06 17:29:36 +0100

    Update layout and grid system in GameDetailPage
    Reconstructed the GameDetailPage to use a SimpleGrid and GridItem from the Chakra UI library. This updated structure allows for better content organization and responsiveness. Game details, including game name, description, and attributes, now appear in one grid item, while the game trailer and screenshots are displayed in a separate grid item.

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame";
-import {Heading, Spinner} from "@chakra-ui/react";
+import {GridItem, Heading, SimpleGrid, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
@@ -13,13 +13,17 @@ const GameDetailPage = () => {
     if (isLoading) return <Spinner/>
     if (error || !game) throw error
     return (
-        <>
-            <Heading>{game.name}</Heading>
-            <ExpandableText>{game.description_raw}</ExpandableText>
-            <GameAttributes game={game}/>
-            <GameTrailer gameId={game.id}/>
-            <GameScreeshots gameId={game.id}/>
-        </>
+        <SimpleGrid columns={{base: 1, md: 2}} spacing={5}>
+            <GridItem>
+                <Heading>{game.name}</Heading>
+                <ExpandableText>{game.description_raw}</ExpandableText>
+                <GameAttributes game={game}/>
+            </GridItem>
+            <GridItem>
+                <GameTrailer gameId={game.id}/>
+                <GameScreeshots gameId={game.id}/>
+            </GridItem>
+        </SimpleGrid>
     );
 };
 
